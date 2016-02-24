8940b01678144951e65a684672bab3e0a009a1e0
YPC <949688912@qq.com>
2016-02-24 22:05:06 +0800

    添加java语法部分:内部类,反射

---
+++ src/main/java/xyz/antsgroup/example/grammar/innerClass/InnerClassTest.java
@@ -0,0 +1,107 @@
+package xyz.antsgroup.example.grammar.innerClass;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.Timer;
+
+/**
+ * Created by ants_ypc on 2/24/16.
+ *
+ * 1.内部类既可以访问自身的数据域,也可以访问创建他的对象的数据域.
+ * 2.局部内部类:类的定义在一个方法中时.这样,只有该方法知道该类的存在.
+ * 3.匿名内部类:见
+ *
+ * 探究1的实现:
+ * 使用 javap -private className 观察编译器实际生成的代码
+ * javap -private InnerClassTest.class
+ * Compiled from "InnerClassTest.java"
+ * public class xyz.antsgroup.example.grammar.innerClass.InnerClassTest {
+ * public xyz.antsgroup.example.grammar.innerClass.InnerClassTest();
+ * public static void main(java.lang.String[]);
+ * }
+ *
+ * javap -private TalkingClock.class
+ * Compiled from "InnerClassTest.java"
+ * class xyz.antsgroup.example.grammar.innerClass.TalkingClock {
+ * private int interval;
+ * private boolean beep;
+ * public xyz.antsgroup.example.grammar.innerClass.TalkingClock(int, boolean);
+ * public void start();
+ * static boolean access$000(xyz.antsgroup.example.grammar.innerClass.TalkingClock);
+ * }
+ *
+ * javap -private TalkingClock\$TimePrinter.class
+ * Compiled from "InnerClassTest.java"
+ * public class xyz.antsgroup.example.grammar.innerClass.TalkingClock$TimePrinter implements java.awt.event.ActionListener {
+ * final xyz.antsgroup.example.grammar.innerClass.TalkingClock this$0;
+ * public xyz.antsgroup.example.grammar.innerClass.TalkingClock$TimePrinter(xyz.antsgroup.example.grammar.innerClass.TalkingClock);
+ * public void actionPerformed(java.awt.event.ActionEvent);
+ * }
+ *
+ * TalkingClock是TimePrinter的外围类,编译器在外围类生成了一个静态方法access$000,在调用内部类是把自身传入.
+ * 编译器给内部类生成了final外围类的引用,这样内部类就可以调用外围类的域了
+ */
+public class InnerClassTest
+{
+    public static void main(String[] args)
+    {
+        TalkingClock clock = new TalkingClock(1000, true);
+
+//        在外部创建内部类对象的方法
+//        TalkingClock.TimePrinter listener = clock.new TimePrinter();
+//        Timer t = new Timer(1000, listener);
+//        t.start();
+
+        clock.start();
+
+        // keep program running until user selects "Ok"
+        JOptionPane.showMessageDialog(null, "Quit program?");
+        System.exit(0);
+
+
+    }
+}
+
+/**
+ * A clock that prints the time in regular intervals.
+ */
+class TalkingClock
+{
+    private int interval;
+    private boolean beep;
+
+    /**
+     * Constructs a talking clock
+     * @param interval the interval between messages (in milliseconds)
+     * @param beep true if the clock should beep
+     */
+    public TalkingClock(int interval, boolean beep)
+    {
+        this.interval = interval;
+        this.beep = beep;
+    }
+
+    /**
+     * Starts the clock.
+     */
+    public void start()
+    {
+        ActionListener listener = new TimePrinter();
+        Timer t = new Timer(interval, listener);
+        t.start();
+    }
+
+
+    public class TimePrinter implements ActionListener
+    {
+        public void actionPerformed(ActionEvent event)
+        {
+            Date now = new Date();
+            System.out.println("At the tone, the time is " + now);
+            // 内部类既可以访问自身的数据域,也可以访问创建他的对象的数据域
+            if (beep) Toolkit.getDefaultToolkit().beep();
+        }
+    }
+}
---
+++ src/main/java/xyz/antsgroup/example/grammar/innerClass/anonymousInnerClass/AnonymousInnerClassTest.java
@@ -0,0 +1,75 @@
+package xyz.antsgroup.example.grammar.innerClass.anonymousInnerClass;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.Timer;
+
+/**
+ * Created by ants_ypc on 2/24/16.
+ *
+ * 由于构造器与类名相同,而匿名类没有类名,所以匿名类没有构造器.取而代之的是把超类的构造器传给他.
+ *
+ * 两种情况使用匿名内部类很好:
+ * 1.该类只有此处使用;
+ * 2.一个对象只使用一次:如添加多个元素的List对象:
+ *   ArrayList<String> friends = new ArrayList<>();
+ *   friends.add("Ha");
+ *   friends.add("Sh");
+ *   invite(friends);
+ *   改写为:
+ *   invite(new ArrayList<String>() {{add("Ha"); add("Sh");}});
+ *   上面两层括号,第一层代表一个子类.第二层括号相当于对象构造块:对象构造的时候一次执行该类的static{},{}...区域的代码.
+ */
+
+public class AnonymousInnerClassTest
+{
+    public static void main(String[] args) {
+        int outInt = 4;
+        Bx bx = new Bx() {
+            @Override
+            public String get(final int outInt) { // 此处的final也可以不加
+                return "get " + outInt + ",匿名内部类使用外部的参数需要使用final.";
+            }
+        };
+        System.out.println(bx.get(4));
+
+        TalkingClock clock = new TalkingClock();
+        clock.start(1000, true);
+
+        // keep program running until user selects "Ok"
+        JOptionPane.showMessageDialog(null, "Quit program?");
+        System.exit(0);
+    }
+}
+
+/**
+ * A clock that prints the time in regular intervals.
+ */
+class TalkingClock
+{
+    /**
+     * Starts the clock.
+     * @param interval the interval between messages (in milliseconds)
+     * @param beep true if the clock should beep
+     */
+    public void start(int interval, boolean beep)
+    {
+        ActionListener listener = new ActionListener()
+        {
+            public void actionPerformed(ActionEvent event)
+            {
+                Date now = new Date();
+                System.out.println("At the tone, the time is " + now);
+                if (beep) Toolkit.getDefaultToolkit().beep();
+            }
+        };
+        Timer t = new Timer(interval, listener);
+        t.start();
+    }
+}
+
+interface Bx {
+    String get(int i);
+}
