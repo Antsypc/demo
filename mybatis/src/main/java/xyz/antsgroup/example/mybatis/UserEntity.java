17761d1ac89babb8ed5e5e6dd71760b11004d9b0
YPC <949688912@qq.com>
2016-04-05 15:59:50 +0800

    更新 mybatis 简单示例

---
+++ mybatis/src/main/java/xyz/antsgroup/example/mybatis/UserEntity.java
@@ -0,0 +1,53 @@
+package xyz.antsgroup.example.mybatis;
+
+/**
+ * @author ants_ypc
+ * @version 1.0 4/5/16
+ */
+public class UserEntity {
+    private int id;
+    private String name;
+    private int age;
+
+    public UserEntity() {
+    }
+
+    public UserEntity(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "UserEntity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
