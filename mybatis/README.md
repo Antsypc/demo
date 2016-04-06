e66fafb25d2aa3fdae3d471a07fccedce229e4f9
YPC <949688912@qq.com>
2016-04-06 17:44:45 +0800

    module mybatis 补充修改部分内容

---
+++ mybatis/README.md
@@ -0,0 +1,3 @@
+MyBatis
+=======
+参考文档: [mybatis](http://www.mybatis.org/mybatis-3/zh/index.html)
--- mybatis/src/main/java/xyz/antsgroup/demo/mybatis/MybatisTest.java
+++ mybatis/src/main/java/xyz/antsgroup/demo/mybatis/MybatisTest.java
@@ -17,8 +17,8 @@ public class MybatisTest {
         String resource = "mybatis-config.xml";
         //使用类加载器加载mybatis的配置文件（它也加载关联的映射文件）
         InputStream is = MybatisTest.class.getClassLoader().getResourceAsStream(resource);
-        //构建sqlSession的工厂
-        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(is);
+        //构建sqlSession的工厂,使用 id=development 的配置
+        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(is, "development");
         //使用MyBatis提供的Resources类加载mybatis的配置文件（它也加载关联的映射文件）
         //Reader reader = Resources.getResourceAsReader(resource);
         //构建sqlSession的工厂
