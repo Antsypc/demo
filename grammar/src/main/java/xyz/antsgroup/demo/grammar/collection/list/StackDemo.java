2f2d20795212444f7b95e4d2275c555a9833f6a4
Antsypc <949688912@qq.com>
2016-04-29 14:47:37 +0800

    Stack 继承自 Vector,线程安全的栈

---
+++ grammar/src/main/java/xyz/antsgroup/demo/grammar/collection/list/StackDemo.java
@@ -0,0 +1,24 @@
+package xyz.antsgroup.demo.grammar.collection.list;
+
+import java.util.Stack;
+
+/**
+ * Stack 继承自 Vector,线程安全的栈.
+ * @author ants_ypc
+ * @version 1.0 4/29/16
+ */
+public class StackDemo {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();    // 只能初始化为空栈
+
+        /**
+         * 除了 Vector 的方法外,额外有下面功能
+         */
+                         stack.push("ad");
+        String element = stack.peek();  // 没有则抛出异常
+               element = stack.pop();   // 没有则抛出异常
+        boolean isEmpty= stack.empty();
+        int loc = stack.search("ad");   // 没有则返回 -1
+
+    }
+}
