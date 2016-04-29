4782f404d74150367a13ab532f3c3452f3481e6b
Antsypc <949688912@qq.com>
2016-04-29 18:31:27 +0800

    优先队列,堆实现,可直接获得最大或最小值,有序

--- grammar/src/main/java/xyz/antsgroup/demo/grammar/collection/queue/PriorityQueueDemo.java
+++ grammar/src/main/java/xyz/antsgroup/demo/grammar/collection/queue/PriorityQueueDemo.java
@@ -4,30 +4,52 @@ import java.util.*;
 
 /**
  * 优先队列 PriorityQueue 相当与堆结构.
- * 始终返回最小的元素,如果需要始终返回最大的元素,在构造是传入 Comparator 接口的方法.
+ * 1. 始终返回最小的元素,如果需要始终返回最大的元素,在构造是传入 Comparator 接口的对象.
+ * 2. 由于需要比较大小,不允许传入 null.
  *
  * 基本队列是Queue接口,LinkedList也是实现了这个接口的,优先级队列PriorityQueue.
  * 双端队列接口Deque,实现了该接口的类有LinkedList,ArrayDeque...
  * 由此看出,需要使用队列时,LinkedList 是很好的选择
  *
- * 队列!元素存取的操作方法:
- * 1. 如果队列是用来做线程管理(如生产者消费者中的队列通道),使用put,take方法.当队列满或者空,该线程将会被阻塞等待.
- * 2. 无异常的存取方法:offer,poll.当队列满或空时,返回false,null.
- * 3. 有异常的存取方法:add,remove.(不推荐)
- * 4. 只是用来获取头元素:element空则有异常,peek空则是null.
- * 注意:poll和peek是通过判断null来判断队列是否为空,所以通过这种方式存取元素就不要存null.
  */
 public class PriorityQueueDemo
 {
     public static void main(String[] args)
     {
+        /**
+         * 构造方法
+         */
         PriorityQueue<GregorianCalendar> pq = new PriorityQueue<>();
+        // 自定义大小比较规则
+        @SuppressWarnings("unchecked")
+        PriorityQueue<String> pq0 = new PriorityQueue<>(new Comparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                return ((String)o2).compareTo((String)o1);
+            }
+        });
         pq.add(new GregorianCalendar(1906, Calendar.DECEMBER, 9)); // G. Hopper
         pq.add(new GregorianCalendar(1815, Calendar.DECEMBER, 10)); // A. Lovelace
         pq.add(new GregorianCalendar(1903, Calendar.DECEMBER, 3)); // J. von Neumann
         pq.add(new GregorianCalendar(1910, Calendar.JUNE, 22)); // K. Zuse
 
-        // 遍历不保证顺序
+        /**
+         * 增删改查
+         * 由于继承 AbstractQueue, 只能向尾部加,从首部获取或删除
+         */
+        // 添加
+        pq0.add("abc");     // 本质是 offer
+        pq0.offer("dfs");   // 不允许空元素的添加
+        // 获取
+        String a = pq0.peek();  // 没有则返回 null
+        // 获取并删除
+        String b = pq0.poll();  // 没有则返回 null
+
+
+
+        /**
+         * 遍历 : 不保证顺序
+         */
         System.out.println("Iterating over elements...");
         for (GregorianCalendar date : pq)
             System.out.println(date.get(Calendar.YEAR));
@@ -36,5 +58,6 @@ public class PriorityQueueDemo
         System.out.println("Removing elements...");
         while (!pq.isEmpty())
             System.out.println(pq.remove().get(Calendar.YEAR));
+
     }
 }
