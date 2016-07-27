447f69bcab0829b2d5b45a853476ad075ea8e791
Antsypc <949688912@qq.com>
2016-07-27 21:00:50 +0800

    aop annotation

--- spring/src/main/java/xyz/antsgroup/demo/spring/aop/aspectj/PreAspect.java
+++ spring/src/main/java/xyz/antsgroup/demo/spring/aop/aspectj/PreAspect.java
@@ -2,12 +2,14 @@ package xyz.antsgroup.demo.spring.aop.aspectj;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
 
 /**
  * 使用注解配置前置增强
  * Created by Ants Young on 2016/7/15.
  */
 @Aspect
+@Component
 public class PreAspect {
 
     /**
